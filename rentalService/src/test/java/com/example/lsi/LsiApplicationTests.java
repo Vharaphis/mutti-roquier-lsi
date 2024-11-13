47faff99adb440d33fe16ec0460aed09ee183fcb
charroux <benoit.charroux@gmail.com>
2024-11-13 14:57:36 +0100

    first commit

---
+++ rentalService/src/test/java/com/example/lsi/LsiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LsiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
